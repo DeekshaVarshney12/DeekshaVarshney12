f1af1e16444f026e0fb0dc597eafcb71eeea007e
DeekshaVarshney12 <103417247+DeekshaVarshney12@users.noreply.github.com>
2022-12-16 12:15:08 +0000

    Update provider.tf

--- demoaks/provider.tf
+++ demoaks/provider.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "csg1003200249f63deb"
+    storage_account_name = "cs11003200258792533"
     container_name       = "mycontainer"
     key                  = "prod.terraform.tfstate"
     access_key = "LOFabw0njaQfwlvWZ7yo5tuiF96M4/SLdHmapX+8JpFlRZSqpPbCClutuB/BCznpbfRcfLuszWhi+AStfEau5Q=="
